d1fd08c98dae0dd953b2f5c0bafa77034c74767b
Jheinel-Brown <109928519+Black-Brown@users.noreply.github.com>
2025-04-12 12:41:31 -0400

    LISTOOOO

--- CineFansApp/src/CineFansApp.Infrastructure/Repositories/IUserRepository.cs
+++ CineFansApp/src/CineFansApp.Infrastructure/Repositories/IUserRepository.cs
@@ -8,8 +8,8 @@ namespace CineFansApp.Infrastructure.Repositories
     {
         Task<User?> GetByEmailAsync(string email);
         Task<List<User>> GetSuggestedUsersAsync(int userId, int count);
-        Task<List<User>> GetFollowersAsync(int userId);
-        Task<List<User>> GetFollowingAsync(int userId);
+        Task<List<User?>> GetFollowersAsync(int userId);
+        Task<List<User?>> GetFollowingAsync(int userId);
         Task<bool> IsFollowingAsync(int followerId, int followedId);
         Task FollowUserAsync(int followerId, int followedId);
         Task UnfollowUserAsync(int followerId, int followedId);
