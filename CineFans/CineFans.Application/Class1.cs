cdf9e0a8bcf233257d76ecb3a0e23a0a78b06d1b
Jheinel-Brown <109928519+Black-Brown@users.noreply.github.com>
2025-04-18 16:23:34 -0400

    el principio

---
+++ CineFans/CineFans.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CineFans.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CineFans/CineFans.Domain/Interface/IUsersRepository.cs
@@ -0,0 +1,14 @@
+﻿using CineFans.Domain.Entities;
+
+namespace CineFans.Domain.Interface
+{
+    internal interface IUsersRepository
+    {
+        Task<Users> GetUserByIdAsync(int userId);
+        Task<Users> GetUserByEmailAsync(string email);
+        Task<IEnumerable<Users>> GetAllAsync();
+        Task<Users> AddAsync(Users user);
+        Task<Users> UpdateAsync(Users user);
+        Task<bool> DeleteAsync(int userId);
+    }
+}
