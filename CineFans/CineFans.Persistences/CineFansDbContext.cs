9493e4e187b93df28c2541822941bb146bca30b7
Jheinel-Brown <109928519+Black-Brown@users.noreply.github.com>
2025-04-19 11:02:19 -0400

    Adding Files

---
+++ CineFans/CineFans.Persistences/CineFansDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CineFans.Domain.Entities;
+
+    public class CineFansDbContext : DbContext
+    {
+        public CineFansDbContext (DbContextOptions<CineFansDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CineFans.Domain.Entities.User> Users { get; set; } = default!;
+
+        public DbSet<CineFans.Domain.Entities.Movie> Movies { get; set; } = default!;
+
+        public DbSet<CineFans.Domain.Entities.Genre> Genres { get; set; } = default!;
+
+        public DbSet<CineFans.Domain.Entities.Post> Posts { get; set; } = default!;
+
+        public DbSet<CineFans.Domain.Entities.Comment> Comments { get; set; } = default!;
+
+        public DbSet<CineFans.Domain.Entities.Follower> Followers { get; set; } = default!;
+
+        public DbSet<CineFans.Domain.Entities.Like> Likes { get; set; } = default!;
+
+    }
